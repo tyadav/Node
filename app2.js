c11c6164a7a09c476a9e4e2bd81a7696602c9c7c
Tej <tej.narayan@gmail.com>
2020-09-02 12:23:00 +0530

    Update app2.js

--- app2.js
+++ app2.js
@@ -2,7 +2,7 @@ const chalk = require('chalk')
 const yargs = require('yargs')
 const getNotes = require('./notes.js')
 
-// Customize yargs version
+//Customize yargs version
 yargs.version('1.1.0')
 
 // Create add command
