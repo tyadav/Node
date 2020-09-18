1d03c3871eb4bf23901a732753497c728a87227d
Tej <tej.narayan@gmail.com>
2020-09-18 12:43:11 +0530

    Add files via upload

---
+++ play4.js
@@ -0,0 +1,26 @@
+// Asynchronous code
+
+const fetchData = callback => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done');
+        }, 1500);  
+    });
+    return promise;
+};
+
+
+setTimeout(() => {
+    console.log('Timer is done');
+    fetchData()
+       .then(text => {
+         console.log(text);
+         return fetchData();
+    })
+    .then(text2 => {
+        console.log(text2);
+    });
+}, 2000);
+
+console.log('Hello!');
+console.log('Hi');
