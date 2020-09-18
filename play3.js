b111e8d22288cf08bafa6f07560a581b5c410bad
Tej <tej.narayan@gmail.com>
2020-09-18 12:42:33 +0530

    Add files via upload

---
+++ play3.js
@@ -0,0 +1,20 @@
+const person = {
+    name: 'Max',
+    age: 29,
+    greet() {
+        console.log('Hi, I am ' + this.name);
+    }
+};
+
+const printName = (personData) => {
+    console.log(personData.name);
+}
+
+printName(person);
+
+const {name, age } = person;
+console.log(name, age);
+
+const hobbies = ['Sports', 'Cooking'];
+const [hobby1, hobby2] = hobbies;
+console.log(hobby1, hobby2);
